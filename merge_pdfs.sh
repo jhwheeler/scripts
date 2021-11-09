6bbe010b5d5083da62dcd4fa645a8f1f05c13209
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2021-11-09 22:26:04 +0800

    simple merge command with a string of file names

---
+++ merge_pdfs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+params="p1.pdf p2.pdf p3.pdf p4.pdf p5.pdf p6.pdf p7.pdf p8.pdf p9.pdf p10.pdf p11.pdf p12.pdf p13.pdf p14.pdf p15.pdf p16.pdf p17.pdf p18.pdf p19.pdf p20.pdf p21.pdf p22.pdf p23.pdf p24.pdf p25.pdf p26.pdf p27.pdf p28.pdf p29.pdf p30.pdf p31.pdf p32.pdf p33.pdf p34.pdf p35.pdf p36.pdf p37.pdf p38.pdf p39.pdf p40.pdf p41.pdf p42.pdf p43.pdf p44.pdf p45.pdf p46.pdf p47.pdf p48.pdf p49.pdf p50.pdf p51.pdf p52.pdf p53.pdf p54.pdf p55.pdf p56.pdf p57.pdf p58.pdf p59.pdf p60.pdf p61.pdf p62.pdf p63.pdf p64.pdf p65.pdf p66.pdf p67.pdf p68.pdf p69.pdf p70.pdf p71.pdf p72.pdf p73.pdf p74.pdf p75.pdf p76.pdf p77.pdf p78.pdf p79.pdf p80.pdf p81.pdf p82.pdf p83.pdf p84.pdf p85.pdf p86.pdf"
+
+pdftk ${params} cat output book.pdf
