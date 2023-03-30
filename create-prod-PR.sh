46e73977818aeb20c9aadfc211e6731ab6407816
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2023-03-30 20:15:31 +0800

    chore: remove unneeded script
    you can just do `git checkout main && update-working-branch production`

--- create-prod-PR.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Update main and production in preparation for prod PR
-
-git checkout main && git pull && git checkout production && git pull && git checkout main
-
-# TODO: prod PR template
